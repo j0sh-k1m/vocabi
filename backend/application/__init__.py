1de2dd3a686cb22b6c7ddbddf31d4c445f52e010
Joshua Kim <74390850+j0sh-k1m@users.noreply.github.com>
2023-08-20 18:28:04 -0400

    Created Tables

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -2,8 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 db = SQLAlchemy()
 Session = sessionmaker()
+Base = declarative_base()
 
 # TODO: Create app using Config
---
+++ backend/application/models/models.py
@@ -0,0 +1,104 @@
+from application import Base
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Boolean, Table, DateTime
+from sqlalchemy.orm import relationship
+from datetime import datetime
+
+
+# Association Tables 
+
+User_Group = Table('user_group', Base.metadata, 
+                   Column('user_id', Integer, ForeignKey('user.user_id')), 
+                   Column('group_id', Integer, ForeignKey('group.group_id'))
+                   )
+
+# End of Association Tables 
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    user_id = Column(Integer, primary_key=True)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    created_at = Column(String, nullable=False)
+    
+    groups = relationship('Group', seconday=User_Group, backref='members', lazy=True)
+    words = relationship('UserWord', backref='user', lazy=True)
+    stats = relationship('UserStat', backref='user', lazy=True)
+
+    def __init__(self, first_name: str, last_name: str, email: str, password: str) -> None:
+        self.first_name = first_name 
+        self.last_name = last_name 
+        self.email = email 
+        self.password = password 
+        self.created_at = datetime.utcnow() 
+
+class Word(Base): 
+    __tablename__ = 'word'
+    word_id = Column(Integer, primary_key=True)
+    word = Column(String, nullable=False)
+    word_type = Column(String, nullable=False)
+    description = Column(String(length=50), nullable=False)
+    definition = Column(String(length=300), nullable=False)
+    created_at = Column(String, nullable=False)
+    
+    def __init__(self, word: str, word_type: str, description: str, definition: str) -> None: 
+        self.word = word
+        self.word_type = word_type 
+        self.description = description 
+        self.definition = definition 
+        self.created_at = datetime.utcnow() 
+
+class UserWord(Base):
+    __tablename__ = 'user_word'
+    word_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    word = Column(String, nullable=False)
+    word_type = Column(String, nullable=False)
+    description = Column(String(length=50), nullable=False)
+    definition = Column(String(length=300), nullable=False)
+    created_at = Column(String, nullable=False)
+    correct = Column(String, nullable=False)
+    incorrect = Column(String, nullable=False)
+
+    def __init__(self, word: str, word_type: str, description: str, definition: str) -> None:
+        self.word = word
+        self.word_type = word_type 
+        self.description = description 
+        self.definition = definition  
+        self.created_at = datetime.utcnow()
+        self.correct = 0 
+        self.incorrect = 0 
+
+    def set_correct_incorrect(self, correct: int, incorrect: int) -> None: 
+        self.correct = correct 
+        self.incorrect = incorrect 
+
+    def increment_correct(self) -> None:
+        self.correct += 1 
+
+    def increment_incorrect(self) -> None:
+        self.incorrect += 1        
+
+class UserStat(Base): 
+    __tablename__ = 'user_stat'
+
+    stat_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+
+    challenge_wins = Column(Integer, nullable=False)
+    total_words_added = Column(Integer, nullable=False)
+    total_words_practiced = Column(Integer, nullable=False)
+    correct = Column(Integer, nullable=False)
+    incorrect = Column(Integer, nullable=False)
+
+
+
+class Group(Base):
+    pass 
+
+class Category(Base):
+    pass 
+
